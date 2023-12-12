9c3bf54d607a30e7d8c6bcfd14b9dbdc5928a6eb
Tayib <bouzekri.tayib@headn.fr>
2023-12-12 17:44:33 +0100

    page_log fonctionne

--- page_log.php
+++ page_log.php
@@ -10,18 +10,15 @@
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="Style_Header.css" />
+    <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
     <div class="container">
       <header>
         <a href="main.php"><img src="images/logo.png" alt="LOGO" /></a>
         <h1 class="ms-4">App Concerts</h1>
-        <button id="btn-default" class="btn btn-outline-primary me-5 pr-5">
-          Par défaut
-        </button>
       </header>
-      <form action="page_log.html" method="post">
+      <form action="page_log.php" method="post">
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label"
             >Adresse e-mail</label
@@ -46,11 +43,38 @@
             placeholder="Veuillez renseigner votre mot de passe."
           />
         </div>
-        <button class="btn btn-primary btn-lg md-4" id="btn-SeConnecter">
-          Me connecter
-        </button>
+        <input type="submit" class="btn btn-primary btn-lg md-4" value="Me connnecter">
       </form>
     </div>
+
+    <?php
+
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+
+    echo "<br> <p class='text-center'> $email $pass </p> <br>";
+
+    $db = new PDO('mysql:host=localhost;dbname=projet_concert;charset=utf8mb4','root','');
+
+    $data = $db->query('SELECT * FROM user')->fetchAll() ;
+    $success=false;
+
+    foreach ($data as $row) {
+        if ($row['email'] == $email and $row['mot_de_passe'] == $pass ) {
+          $success = true; 
+          break;
+        }
+
+      }
+
+        if ($success) {
+          header('Location: page_ajout_salle.php');
+          echo "<br> <p class='text-center'>Hi ".$row['nom']." you have been logged in. </p> <br>";
+          } else {
+          echo "<br> <p class='text-center'> You have entered the wrong username or password. Please try again. </p> <br>";
+
+        }
+    ?>
 
     <script>
       src =
